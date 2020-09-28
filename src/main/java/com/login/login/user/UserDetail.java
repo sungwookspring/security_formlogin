a4ea1b32f20ac1b26fd048c00e61ae03964502f7
choisungwook <kgg1959@naver.com>
2020-09-28 10:06:41 +0900

    Add UserDetail

---
+++ src/main/java/com/login/login/user/UserDetail.java
@@ -0,0 +1,53 @@
+package com.login.login.user;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@NoArgsConstructor
+public class UserDetail implements UserDetails {
+    private DBUser user;
+
+    @Builder
+    public UserDetail(DBUser user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
